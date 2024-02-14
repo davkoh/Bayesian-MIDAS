3b9884c96daf3ea67f88a103b86bc36456467329
davkoh <david.kohns94@googlemail.com>
2024-02-14 09:57:50 +0200

    Initial results

--- R&R/bmidas_hspriors_sw.m
+++ R&R/bmidas_hspriors_sw.m
@@ -1,7 +1,7 @@
 %% Function to do Inference with the HS on the Trend and SV %%
 
 
-function out = bmidas(input)
+function out = bmidas_hspriors(input)
 
 %% Unpack Data from Input Structure
 % General Data
@@ -37,12 +37,18 @@ data.iOh = eye(T);
 prior_tau.V_t0 = 10;
 prior_tau.H = speye(T) - sparse(2:T,1:(T-1),ones(1,T-1),T,T);
 
+% Data structure for the h sampler
+prior_sv_h.Vh = 10;
+% Data structure for the g sampler
+prior_sv_g.Vh = 10;
+
+
 %% Initialise Unknowns
 
     % For the beta sampler
     hyper_params.tau_sq = 1;
     hyper_params.nu = 5;
-    hyper_params.gamma_sq = ones(G,1);
+    hyper_params.gamma_sq = ones(size(unique(input.grp_idx),1),1);
     hyper_params.lambda_sq = ones(K,1);
 
     % For the Trend
@@ -51,6 +57,13 @@ prior_tau.H = speye(T) - sparse(2:T,1:(T-1),ones(1,T-1),T,T);
     tau0 = 0; % Initial condition
     lambdatau = ones(T,1); % Local shrinkage on trend
     nutau = 1; % Global shrinkage on trend
+
+    g = ones(T,1);
+    g0 = 0;
+    Sigmag = ones(T,1);
+    lambdag  = ones(T,1);
+    nug = 1;
+    a0_tau = 0; b0_tau = 10;
 
     % For Stochastic vol
     h = ones(T,1); % svol
@@ -64,10 +77,12 @@ prior_tau.H = speye(T) - sparse(2:T,1:(T-1),ones(1,T-1),T,T);
     lamt = ones(T,1); % mixture for t-distribution
     nut = 5; % dof
 
+    H = speye(T) - sparse(2:T,1:(T-1),ones(1,T-1),T,T);
+
 
 %% Storage Matrices
 beta_store = zeros(K,n_samples);
-gamma_store = zeros(G,n_samples);
+gamma_store = zeros(size(unique(input.grp_idx),1),n_samples);
 tausq_store = zeros(n_samples,1);
 tau_store = zeros(T,n_samples);
 store_h = zeros(T,n_samples);
@@ -88,25 +103,34 @@ lambda_store = zeros(T,n_samples);
 %
 for loops = 1:n_burn_in+n_samples
 %% Sample beta
-data.Y = Y-tau;
+data.Y = Y;
 data.iOh = 1./(exp(h).*lamt).*speye(T);
 [beta,hyper_params,data] = sample_beta(data,hyper_params);
 
 %% Sample Tau
-[tau,Sigmatau,tau0,lambdatau,nutau] = sample_trend_unitroot_hs(Y-X*beta,tau,Sigmatau,tau0,lambdatau,nutau,exp(h).*lamt,prior_tau);
+iOh = sparse(1:T,1:T,1./(exp(h).*lamt));
+HiOgH = H'*sparse(1:T,1:T,1./exp(g))*H;
+Ktau =  HiOgH + iOh;    
+tau_hat = Ktau\(tau0*HiOgH*ones(T,1) + iOh*(Y-tau));
+tau = tau_hat + chol(Ktau,'lower')'\randn(T,1);
+
+%% Sample g
+ystar = log((tau-[tau0;tau(1:end-1)]).^2 + .0001);
+[g,Sigmag,g0,lambdag,nug] = sample_sv_unitroot_hs(ystar,g,Sigmag,g0,lambdag,nug,prior_sv_g);
+
+% Sample initial state:
+    Ktau0 = 1/b0_tau + 1/exp(g(1));
+    tau0_hat = Ktau0\(a0_tau/b0_tau + tau(1)/exp(g(1)));
+    tau0 = tau0_hat + sqrt(Ktau0)'\randn;
 
 %% Sample h
-u = y-tau-X*beta;
+u = Y-tau-X*beta;
 Ystar = log(u.^2.*lamt.^(-1) + .0001);
-[h,Sigmah,h0,lambdah,nuh] = sample_sv_unitroot_hs(Ystar,h,Sigmah,h0,lambdah,nuh,prior_sv);
-
-%% Sample tau0
-Ktau0 = 1/b0_tau + 1/exp(g(1));
-tau0_hat = Ktau0\(a0_tau/b0_tau + tau(1)/exp(g(1)));
-tau0 = tau0_hat + sqrt(Ktau0)'\randn;
+[h,Sigmah,h0,lambdah,nuh] = sample_sv_unitroot_hs(Ystar,h,Sigmah,h0,lambdah,nuh,prior_sv_h);
 
 %% Sample t-errors
 [lamt,nut] = sample_t_errors(u,lamt,nut,nub,exp(h));
+lamt = ones(T,1);
 
 %% Save output %% Needs adjustment to what is being saved
 
@@ -120,15 +144,13 @@ if loops>n_burn_in
     end
 end
 
-
-end
-
 out.beta =beta_store;
 out.tau = tau_store;
 out.h = store_h;
 out.nu = nuy_store;
 out.lambda = lambda_store;
 out.beta_savs = savs(beta_store',X);
+
 
 
 %% ============== Sampling Functions ============== %% 
